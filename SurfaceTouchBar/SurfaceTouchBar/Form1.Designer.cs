81ed9394db831edf75444c2a1233c36a2fa11ced
Freddy Straub <straub.freddy@web.de>
2017-02-27 10:38:34 +0100

    Display Butons

--- SurfaceTouchBar/SurfaceTouchBar/Form1.Designer.cs
+++ SurfaceTouchBar/SurfaceTouchBar/Form1.Designer.cs
@@ -28,9 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(202, 678);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- SurfaceTouchBar/SurfaceTouchBar/Form1.cs
+++ SurfaceTouchBar/SurfaceTouchBar/Form1.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,5 +19,96 @@ namespace SurfaceTouchBar
         {
             InitializeComponent();
         }
+
+        Screen[] screens;
+        Screen currentScreen;
+
+        Button b1;
+        Button b2;
+        Button b3;
+        Button b4;
+        Button b5;
+        Button b6;
+        Button b7;
+        Button b8;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            screens = Screen.AllScreens;
+            currentScreen = Screen.FromControl(this);
+
+            StartPosition = FormStartPosition.Manual;
+
+            Height = Screen.PrimaryScreen.Bounds.Height  - calculateTaskbarHeight();
+
+            displayButtons();
+
+            b1.Click += new EventHandler(this.b1_Click);
+        }
+
+
+        private void b1_Click(Object sender, EventArgs e)
+        {
+
+
+        }
+
+        /// <summary>
+        /// Creates perfect SurfaceTouchBar - Button.
+        /// </summary>
+        /// <param name="buttonText">Button Text</param>
+        /// <returns>SurfaceTouchBar - Button</returns>
+        private Button generateBarButton(string buttonText)
+        {
+            Button newBarButton = new Button();
+
+            newBarButton.Width = this.Width;
+            newBarButton.Text = buttonText;
+            newBarButton.Height = Height / 8;
+            newBarButton.FlatStyle = FlatStyle.Flat;
+
+            return newBarButton;
+        }
+
+        /// <summary>
+        /// Gibt die Höhe der Taskbar zurück.
+        /// </summary>
+        /// <returns></returns>
+        private static int calculateTaskbarHeight()
+        {
+            return Screen.PrimaryScreen.Bounds.Height - Screen.PrimaryScreen.WorkingArea.Height;
+        }
+
+        /// <summary>
+        /// Dispalys the Buttons.
+        /// </summary>
+        private void displayButtons()
+        {
+            b1 = generateBarButton("Guten Morgen!");
+            Controls.Add(b1);
+            b2 = generateBarButton("Guten Morgen!");
+            b2.Location = new Point(0, Height / 8);
+            Controls.Add(b2);
+            b3 = generateBarButton("Guten Morgen!");
+            b3.Location = new Point(0, (Height / 8) * 2);
+            Controls.Add(b3);
+            b4 = generateBarButton("Guten Morgen!");
+            b4.Location = new Point(0, (Height / 8) * 3);
+            Controls.Add(b4);
+            b5 = generateBarButton("Guten Morgen!");
+            b5.Location = new Point(0, (Height / 8) * 4);
+            Controls.Add(b5);
+            b6 = generateBarButton("Guten Morgen!");
+            b6.Location = new Point(0, (Height / 8) * 5);
+            Controls.Add(b6);
+            b7 = generateBarButton("Guten Morgen!");
+            b7.Location = new Point(0, (Height / 8) * 6);
+            Controls.Add(b7);
+            b8 = generateBarButton("Guten Morgen!");
+            b8.Location = new Point(0, (Height / 8) * 7);
+            Controls.Add(b8);
+        }
+
     }
 }
